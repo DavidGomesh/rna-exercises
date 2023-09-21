b2b8de9cff200f30fae675106ced4b717651523d
David Gomesh <davidgomesh2016@gmail.com>
2023-09-21 12:09:35 -0300

    Remove recursion

---
+++ networks/fruits.py
@@ -0,0 +1,61 @@
+
+from perceptron import Perceptron
+from perceptron import predict
+from perceptron import fit2
+
+inputs = [
+    [0.1, 0.4, 0.7],
+    [0.5, 0.7, 0.1],
+    [0.6, 0.9, 0.8],
+    [0.3, 0.7, 0.2],
+]
+
+targets = [1, 1, -1, -1]
+
+learningRate = 0.05
+
+otherInputs = [
+    [0.2, 0.3, 0.6], 
+    [0.5, 0.8, 0.8], 
+    [0.3, 0.6, 0.4], 
+    [0.2, 0.6, 0.3]
+]
+
+otherTargets = [1, -1, -1, -1]
+
+def test01():
+    EPOCHS = 38
+    stepFn = lambda u: 1 if u >= 0 else -1
+    # weights = [-6, 0.8, -8.6, ]
+    # bias = -3.6
+
+    p = Perceptron(inputs=inputs, targets=targets, learningRate=learningRate, activation=stepFn)
+    p.fit(EPOCHS)
+
+    testInputs(p)
+
+def test02():
+    EPOCHS = 54
+    weights = [-0.241, 0.68, -0.778, 0.015]
+
+def testInputs(p: Perceptron):
+    for i, t in zip(inputs + otherInputs, targets + otherTargets):
+        print(f"Inputs: {i}\tTarget: {t}\tPrediction: {p.predict(i)}")
+
+
+# test01()
+
+# fit2(inputs, targets, )
+
+
+# for i, t in zip(inputs + otherInputs, targets + otherTargets):
+#     print(f"I: {i}\tT: {t}\tP: {predict(i, [0.8, -8.6, -3.6], -6, lambda u: 1 if u >= 0 else -1)}")
+
+
+
+
+
+
+
+
+
--- networks/perceptron.py
+++ networks/perceptron.py
@@ -1,7 +1,4 @@
 
-from sys import setrecursionlimit
-setrecursionlimit(100000)
-
 from random import uniform
 from activation import step
 
@@ -11,23 +8,13 @@ def predict(inputs, weights, bias, activation):
 
 # Fit n epochs
 def fit(inputs, targets, weights, bias, learningRate, activation, epochs):
-    if epochs == 0:
-        return weights, bias
-    
-    weights, bias = fitEpoch(inputs, targets, weights, bias, learningRate, activation)
-    return fit(inputs, targets, weights, bias, learningRate, activation, epochs-1)
-
-# Fit just one epoch
-def fitEpoch(inputs, targets, weights, bias, learningRate, activation):
-    if not inputs:
-        return weights, bias
-    
-    prediction = predict(inputs[0], weights, bias, activation)
-    error = update(targets[0], prediction, learningRate)
-    weights = updatedWeights(inputs[0], weights, error)
-    bias = updatedBias(bias, error)
-    
-    return fitEpoch(inputs[1:], targets[1:], weights, bias, learningRate, activation)
+    for _ in range(epochs):
+        for input, target in zip(inputs, targets):
+            prediction = predict(input, weights, bias, activation)
+            error = update(target, prediction, learningRate)
+            weights = updatedWeights(input, weights, error)
+            bias = updatedBias(bias, error)
+    return weights, bias
 
 # Calculates the update value
 def update(target, prediction, learningRate):
@@ -41,31 +28,23 @@ def updatedWeights(inputs, weights, update):
 def updatedBias(bias, update):
     return bias + update
 
+
 # Perceptron class
 class Perceptron:
     def __init__(self, inputs, targets, weights=[], bias=uniform(-1, 1), learningRate=0.01, activation=step):
-        self.inputs = inputs
-        self.targets = targets
-        self.bias = bias
+        self.weights = weights if weights else [uniform(-1, 1) for _ in range(len(inputs[0]))]
         self.learningRate = learningRate
         self.activation = activation
-        self.weights = weights if weights else [uniform(-1, 1) for _ in range(len(inputs[0]))]
+        self.targets = targets
+        self.inputs = inputs
+        self.bias = bias
 
     def fit(self, epochs=1):
         self.weights, self.bias = fit(
-            self.inputs,
-            self.targets,
-            self.weights,
-            self.bias,
-            self.learningRate,
-            self.activation,
-            epochs
+            self.inputs, self.targets, self.weights, self.bias, self.learningRate, self.activation, epochs
         )
     
     def predict(self, inputs):
         return predict(
-            inputs, 
-            self.weights, 
-            self.bias, 
-            self.activation
+            inputs,  self.weights,  self.bias,  self.activation
         )
